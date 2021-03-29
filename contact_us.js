e88018cf87872212e5a0230d6ea5e7c105bdd932
ARSALAN UMAR <64530489+Arxlan40@users.noreply.github.com>
2021-03-29 18:37:45 +0500

    new

--- contact_us.js
+++ contact_us.js
@@ -20,20 +20,16 @@ class contactDetails {
 }
 
 contactbutton.addEventListener("click", function () {
-    if (contactfieldName.value != "" && contactfieldEmail.value != "" &&   contactfieldMessage.value != "") {
+    if (contactfieldName.value != "" && contactfieldEmail.value != "" && contactfieldMessage.value != "") {
 
-            const usermessage = new contactDetails(contactfieldMessage.value, contactfieldName.value, contactfieldEmail.value,);
-            users.push(usermessage);
-            const usersJson = JSON.stringify(usermessage);
-            localStorage.setItem("usersmessages", usersJson);
+        const usermessage = new contactDetails(contactfieldMessage.value, contactfieldName.value, contactfieldEmail.value,);
+        users.push(usermessage);
+        const usersJson = JSON.stringify(usermessage);
+        localStorage.setItem("usersmessages", usersJson);
 
-            contactfieldName.value = "";
-            contactfieldEmail.value = "";
-            contactfieldMessage.value = '';
-          
-            window.alert("Your message is sent to the admin");
+        window.alert("Your message is sent to the admin");
 
-        
+
     }
     else {
         window.alert("Please fill the contact form before sending message");
